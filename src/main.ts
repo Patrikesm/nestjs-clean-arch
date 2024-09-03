635474b8f1d36056511cb36f02c43c6cd6d6acd8
Patrikesm <patrikemiranda@hotmail.com>
2024-09-02 23:02:42 -0300

    Alterar prettier e eslint

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter());
+    new FastifyAdapter(),
+  );
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
